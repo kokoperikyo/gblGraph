a573a9504b8c491b8154dbc34e394f95ba752156
shane <shane.kokoperi@gmail.com>
2020-05-08 12:51:18 +0900

    firstアクセスのアイコンセット

--- src/components/First.vue
+++ src/components/First.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>first</div>
+  <div class="content">
+    <v-row>
+      <v-col cols="3"></v-col>
+      <v-col cols="6" align="center">
+        <v-img
+          width="220px"
+          src="https://firebasestorage.googleapis.com/v0/b/gblgraph.appspot.com/o/icon%2F%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88%202020-05-08%2012.35.58.png?alt=media&token=b389877a-b0fd-4b7e-977b-662c409ad9c7"
+        ></v-img>
+      </v-col>
+      <v-col cols="3"></v-col>
+    </v-row>
+  </div>
 </template>
 <script>
 export default {
@@ -22,3 +33,8 @@ export default {
   }
 };
 </script>
+<style>
+.content {
+  margin-top: 200px;
+}
+</style>
